a337fb375ce3e349cc880153dd344881a7fecf3e
p4welo <p4welo@gmail.com>
2016-06-09 11:49:26 +0200

    gulp-sass introduced

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    sass = require('gulp-sass');
 
 gulp.task('server', function () {
-    connect.server()
+    connect.server({
+        livereload: true
+    })
 });
 
-gulp.task('default', ['server']);
+gulp.task('sass', function () {
+    gulp.src('assets/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('assets'))
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+    gulp.watch('assets/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'server', 'watch']);
